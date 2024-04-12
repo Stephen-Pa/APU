027287e31a6f3164d1b0e885ef6eb54eb7234224
Greg <sjp109@pitt.edu>
2024-04-12 12:22:59 -0400

    first verison of test script

--- semester2998/svmScript.sh
+++ semester2998/svmScript.sh
@@ -135,3 +135,125 @@ do
         echo "$i">> $outFileTimingBreast
         ./build/debug/gsi_device_lab_3 11 9 699 "./Breast_SVM/Quarter"
 done
+
+echo "Breast 87 9 524" >> $outFileTimingBreast
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingBreast
+        ./build/debug/gsi_device_lab_3 87 9 524 "./Breast_SVM/Full"
+done
+
+echo "Breast 87 9 349" >> $outFileTimingBreast
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingBreast
+        ./build/debug/gsi_device_lab_3 87 9 349 "./Breast_SVM/Full"
+done
+
+echo "Breast 87 9 174" >> $outFileTimingBreast
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingBreast
+        ./build/debug/gsi_device_lab_3 87 9 174 "./Breast_SVM/Full"
+done
+#ALL GAMMA TESTS
+echo "Gamma 17489 10 19020" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 17489 10 19020 "./Gamma_SVM/Full"
+done
+
+echo "Gamma 12730 10 19020" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 12730 10 19020 "./Gamma_SVM/Quarter3"
+done
+
+echo "Gamma 8463 10 19020" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 8463 10 19020 "./Gamma_SVM/Half"
+done
+
+echo "Gamma 4298 10 19020" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 4298 10 19020 "./Gamma_SVM/Quarter"
+done
+
+echo "Gamma 17489 10 14265" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 17489 10 14265 "./Gamma_SVM/Full"
+done
+
+echo "Gamma 17489 10 9510" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 17489 10 9510 "./Gamma_SVM/Full"
+done
+
+echo "Gamma 17489 10 4755" >> $outFileTimingGamma
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingGamma
+        ./build/debug/gsi_device_lab_3 17489 10 4755 "./Gamma_SVM/Full"
+done
+#ALL MINIBOON
+echo "MiniBoon 32659 48 32768" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 32659 48 32768 "./MiniBoon_SVM/Full"
+done
+
+echo "MiniBoon 24499 48 32768" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 24499 48 32768 "./MiniBoon_SVM/Quarter3"
+done
+
+echo "MiniBoon 16324 48 32768" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 16324 48 32768 "./MiniBoon_SVM/Half"
+done
+
+echo "MiniBoon 8162 48 32768" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 8162 48 32768 "./MiniBoon_SVM/Quarter"
+done
+
+echo "MiniBoon 32659 48 24576" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 32659 48 24576 "./MiniBoon_SVM/Full"
+done
+
+echo "MiniBoon 32659 48 16384" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 32659 48 16384 "./MiniBoon_SVM/Full"
+done
+
+echo "MiniBoon 32659 48 8192" >> $outFileTimingMiniBoon
+for i in {1..1000}
+do
+        echo "$i">> $outFileTimingMiniBoon
+        ./build/debug/gsi_device_lab_3 32659 48 8192 "./MiniBoon_SVM/Full"
+done
+
+
+
